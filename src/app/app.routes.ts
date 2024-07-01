34b532b586578eb956095d5bb46364a02795a13b
PaulaVelezz <velezpaula.a@gmail.com>
2024-06-30 21:45:28 -0300

    1.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { TableComponent } from './components/table/table.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'table', component: TableComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
